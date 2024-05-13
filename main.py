75ffbb59e31d910186e93f2b7020f44f2a75c933
rav39439 <ravish.kumar@ispf.ngo>
2024-05-13 23:45:03 +0530

    updated the logic

--- main.py
+++ main.py
@@ -552,3 +552,95 @@ visualize(image, text, train_dataset.tokenizer, axs[0], "Right")
 visualize(gen_image, text, None, axs[1], "Generated")
 plt.show()
 
+
+# //--------------------------------Next word prediction----------------
+
+# import tensorflow as tf
+# import string
+# import requests
+
+# response = requests.get('https://ocw.mit.edu/ans7870/6/6.006/s08/lecturenotes/files/t8.shakespeare.txt')
+
+# response.text[:1500]
+# # print(response.text[:1500])
+# data = response.text.split('\n')
+# data = data[253:]
+
+# data = " ".join(data)
+# data[:1000]
+
+# def clean_text(doc):
+#   tokens = doc.split()
+#   table = str.maketrans('', '', string.punctuation)
+#   tokens = [w.translate(table) for w in tokens]
+#   tokens = [word for word in tokens if word.isalpha()]
+#   tokens = [word.lower() for word in tokens]
+#   return tokens
+
+# tokens = clean_text(data)
+# print(tokens[:50])
+
+# length = 50 + 1
+# lines = []
+
+# for i in range(length, len(tokens)):
+#   seq = tokens[i-length:i]
+#   line = ' '.join(seq)
+#   lines.append(line)
+#   if i > 200000:
+#     break
+
+# print(len(lines))
+
+# import numpy as np
+# from keras.utils import to_categorical
+# from keras.models import Sequential
+# from keras.layers import Dense, LSTM, Embedding
+# from keras_preprocessing.text import Tokenizer
+# from keras_preprocessing.sequence import pad_sequences
+
+
+
+# tokenizer = Tokenizer()
+# tokenizer.fit_on_texts(lines)
+# sequences = tokenizer.texts_to_sequences(lines)
+
+# sequences = np.array(sequences)
+# X, y = sequences[:, :-1], sequences[:,-1]
+
+# vocab_size = len(tokenizer.word_index) + 1
+# y = to_categorical(y, num_classes=vocab_size)
+# seq_length = X.shape[1]
+
+# model = Sequential()
+# model.add(Embedding(vocab_size, 50, input_length=seq_length))
+# model.add(LSTM(100, return_sequences=True))
+# model.add(LSTM(100))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(vocab_size, activation='softmax'))
+# model.summary()
+# model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+# seed_text=lines[12343]
+# # print(seed_text)
+
+# def generate_text_seq(model, tokenizer, text_seq_length, seed_text, n_words):
+#   text = []
+
+#   for _ in range(n_words):
+#     encoded = tokenizer.texts_to_sequences([seed_text])[0]
+#     encoded = pad_sequences([encoded], maxlen = text_seq_length, truncating='pre')
+
+#     # y_predict = model.predict_classes(encoded)
+#     predictions = model.predict(encoded)
+#     y_predict = np.argmax(predictions, axis=-1)[0]
+#     predicted_word = ''
+#     for word, index in tokenizer.word_index.items():
+#       if index == y_predict:
+#         predicted_word = word
+#         break
+#     seed_text = seed_text + ' ' + predicted_word
+#     text.append(predicted_word)
+#   return ' '.join(text)
+
+# print(generate_text_seq(model, tokenizer, seq_length,seed_text, 100))
